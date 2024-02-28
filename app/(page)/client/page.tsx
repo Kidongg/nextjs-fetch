2c21a97ede62cc7f359e4a05345854f376493e91
Kidongg <dlrlehd7@daum.net>
2024-02-29 02:00:09 +0900

    클라이언트 컴포넌트 데이터 패칭

--- app/(page)/client/page.tsx
+++ app/(page)/client/page.tsx
@@ -1,7 +1,45 @@
 "use client";
 
+import DataTable from "@/app/components/table/DataTable";
+import { paymentColumns } from "@/app/components/table/columns";
+import { useEffect, useState } from "react";
+
+const getPayments = async () => {
+  const res = await fetch("http://localhost:3001/payments", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+
+  const data = await res.json();
+  return data;
+};
+
 const Page = () => {
-  return <div>클라이언트 컴포넌트</div>;
+  const [payments, setPayments] = useState([]);
+
+  useEffect(() => {
+    const fetchPayments = async () => {
+      const payments = await getPayments();
+      setPayments(payments);
+    };
+
+    fetchPayments();
+  }, []);
+
+  console.log("payments: ", payments);
+
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-2xl font-bold mb-10">클라이언트 컴포넌트</h1>
+      <DataTable data={payments} columns={paymentColumns} />
+    </div>
+  );
 };
 
 export default Page;
